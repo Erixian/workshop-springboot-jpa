e169f0c13de91eded97f497ac2723b1648a9cb82
Erixian <cibercianocode@gmail.com>
2026-01-22 15:17:46 -0300

    service layer, component registration

---
+++ src/main/java/com/erixan/course/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.erixan.course.repositories;
+
+import com.erixan.course.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
